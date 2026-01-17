b00394bcee7728fd690d860afcdd2556a4e80c10
luisidrm <riosluisdaniel195@gmail.com>
2026-01-17 03:17:46 -0500

    Fixing some details

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import { AppRegistry, Platform } from "react-native";
+import { registerRootComponent } from "expo";
 import App from "./App";
-import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
-
-if (Platform.OS === "web") {
-    const rootTag =
-        document.getElementById("root") || document.getElementById("main");
-    AppRegistry.runApplication(appName, { rootTag });
-}
+registerRootComponent(App);
